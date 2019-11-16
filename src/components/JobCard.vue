c3b729f811c2972411f4dbbe868c4ec16819cf6d
Rui Zhang <rz1@iu.edu>
2019-11-16 12:16:09 -0500

    chore: Display basic information without the hover on the Job Card; Display basic information without the hover on the User Card; Added adaptive colors. fixed issue #6

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -1,36 +1,46 @@
 <template>
   <div class="wrapper">
-      <b-card
-        style="height: 300px; border-radius: 10px;"
-        >
-          <img :src="job.employer.profilepicture ? job.employer.profilepicture.toString() : require('../assets/company.jpg')" class="card-img-top embed-responsive"
-               style="max-height: 299px; height: 100%; border-radius: 10px; object-fit: cover;" />
-      </b-card>
-      <div class="overlay" @click="$emit('showJobModal')">
-        <div class="text">
-          <br />
-          <span style="font-weight: bold">{{job.company}}</span>
-          <br />
-          <span>{{job.location}}</span>
-          <br />
-          <span>{{displaySkills(job.skills)}}</span>
-          <br />
-          <br />
-          <br />
-          <div class="row">
-            <div class="col-6">
-              <button class=" mr-5 btn-lg btn-danger ml-5" @click="$emit('reject', job._id)"><i class="ti-close"></i></button>
-            </div>
-            <div class="col-6">
-              <button @click="$emit('accept', job._id)" class=" mr-4 btn-lg btn-success"><i class="ti-check"></i></button>
-            </div>
+    <b-card
+            style="height: 300px; border-radius: 10px;"
+    >
+      <img
+        :src="job.employer.profilepicture ? job.employer.profilepicture.toString() : require('../assets/company.jpg')"
+        class="card-img-top embed-responsive"
+        style="max-height: 299px; height: 100%; border-radius: 10px; object-fit: cover;"/>
+      <div class="info_background" :style="info_bg"></div>
+      <div class="info">
+        <span style="font-weight: bold">{{job.company}}</span>
+        <br/>
+        <span>{{job.location}}</span>
+        <br/>
+        <span>{{displaySkills(job.skills)}}</span>
+        <br/>
+        <br/>
+        <br/>
+      </div>
+    </b-card>
+    <div class="overlay" :style="info_bg" @click="$emit('showJobModal')">
+      <div class="text">
+        <div class="row">
+          <div class="col-6">
+            <div class=" ml-5">Dismiss</div>
+            <br>
+            <button class=" mr-5 btn-lg btn-danger ml-5" @click="$emit('reject', job._id)" ><i class="ti-close"></i>
+            </button>
+          </div>
+          <div class="col-6">
+            <div class="mr-4 ">Apply</div>
+            <br>
+            <button @click="$emit('accept', job._id)" class=" mr-4 btn-lg btn-success"><i class="ti-check"></i></button>
           </div>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
+import * as Vibrant from 'node-vibrant'
 export default {
   name: 'JobCard',
   data () {
@@ -38,7 +48,10 @@ export default {
       skillsToShow: '',
       imgArray: [
         '../assets/company.jpg',
-      ]
+      ],
+      info_bg:{
+        backgroundColor:"#16a085"
+      }
     }
   },
   props: {
@@ -48,6 +61,16 @@ export default {
       default: {
         skills: []
       }
+    }
+  },
+
+  mounted () {
+    if (this.job.employer.profilepicture) {
+      Vibrant.from(this.job.employer.profilepicture.toString()).getPalette().then(response => {
+        this.info_bg.backgroundColor = response.DarkMuted.getHex()
+      })
+    } else {
+      this.info_bg.backgroundColor = 'rgba(0,0,0,0.57)'
     }
   },
   methods: {
@@ -61,7 +84,7 @@ export default {
       } else {
         return ''
       }
-    }
+    },
   }
 }
 </script>
@@ -78,16 +101,47 @@ export default {
     bottom: 0;
     left: 0;
     right: 0;
-    height: 100%;
+    height: 66.66%;
     width: 100%;
     opacity: 0;
-    border-radius: 10px;
+    /*border-radius: 10px;*/
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
     transition: .5s ease;
-    background-color: #262827;
+    background-color: rgba(38, 40, 39, 0.5);
+  }
+  .info_background{
+    position: absolute;
+    top: 66.7%;
+    left: -0.3%;
+    height: 33.3%;
+    width: 100.6%;
+    text-align: center;
+    opacity: .9;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    background-color: rgba(2, 117, 216, 0.78);
+  }
+
+  .info {
+    color: white;
+    font-size: 20px;
+    position: absolute;
+    top: 68%;
+    left: 0;
+    height: 33.3%;
+    width: 100%;
+    text-align: center;
   }
 
   .wrapper:hover .overlay {
-    opacity: 0.98;
+    opacity: .9;
+  }
+
+  .btn-lg {
+    border-radius: 100px;
+    width: 60px;
+    height: 60px;
   }
 
   .text {
@@ -96,6 +150,8 @@ export default {
     position: relative;
     top: 50%;
     left: 50%;
+    opacity: 0.98;
+
     -webkit-transform: translate(-50%, -50%);
     -ms-transform: translate(-50%, -50%);
     transform: translate(-50%, -50%);
