f9c1c42a42cd8057ba58b1a289a59359150f25d8
Rui Zhang <rz1@iu.edu>
2019-11-17 18:39:08 -0500

    feat: added security question field in the register tab, changed space between each field, made the password requirement on display when password input box has been clicked, in progress of fixing issue #4 and issue #12

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -5,41 +5,39 @@
     <div v-if="error">
         <b-alert show variant="danger">{{error}}</b-alert>
     </div>
-    <div class="text-center mt-4">
-      <a class="btn btn-md linkedin-btn light" @click="$emit('linkedin')" style="color: white;">Linked <i class="ti-linkedin"></i></a>
-    </div>
-    <div class="center-separator my-3 mt-4">
-      or
-    </div>
+<!--    <div class="text-center mt-4">-->
+<!--      <a class="btn btn-md linkedin-btn light" @click="$emit('linkedin')" style="color: white;">Linked <i class="ti-linkedin"></i></a>-->
+<!--    </div>-->
+<!--    <div class="center-separator my-3 mt-4">-->
+<!--      or-->
+<!--    </div>-->
     <b-form @submit="onSubmit" class="p3">
-      <label class="smaller-font">Email</label>
-      <b-form-group id="fieldsetHorizontal">
+      <label class="smaller-font" style="padding-top: 0;margin-top: 0">Email</label>
         <b-form-input id="username" v-model.trim="register.username" class="no-border"></b-form-input>
-      </b-form-group>
-      <label class="smaller-font">Password</label><span></span>
-      <b-form-group id="fieldsetHorizontal" class="mb-0"
-                    >
-        <b-form-input type="password" id="password" :state="state" v-model.trim="register.password" class="no-border"></b-form-input>
-      </b-form-group>
-      <div style="font-size: 11px; padding-top: 0; margin-bottom: 10px !important; margin-top: 2px;">Must be at least 8 characters, have an uppercase letter and a special character</div>
+
+      <label class="smaller-font" >Password</label><span></span>
+        <b-form-input @click="showPassRequirement" type="password" id="password" :state="state" v-model.trim="register.password" class="no-border"></b-form-input>
+      <div v-if="on_typing" style="font-size: 11px; padding-top: 0; margin-bottom: 0px !important; margin-top: 2px;">Must be at least 8 characters, have an uppercase letter and a special character</div>
+
       <label class="smaller-font">Re-Enter Password</label>
-      <b-form-group id="fieldsetHorizontal"
-                    >
         <b-form-input type="password" id="password2" :state="state" v-model.trim="register.password2" class="no-border"></b-form-input>
-      </b-form-group>
 
       <label class="smaller-font">Select Role</label>
-      <b-form-group
-        id="fieldsetHorizontal">
         <b-form-select v-model="register.role" :options="options" size="md" id="role" class="w-100"></b-form-select>
-      </b-form-group>
 
-      <label class="smaller-font" v-if="register.role == 'student'">Gender</label>
-      <b-form-group
-        v-if="register.role === 'student'"
-        id="fieldsetHorizontal">
-        <b-form-select v-model="register.gender" :options="genderOptions" size="md" id="role" class="w-100"></b-form-select>
-      </b-form-group>
+<!--      <label class="smaller-font" v-if="register.role == 'student'">Gender</label>-->
+<!--      <b-form-group-->
+<!--        v-if="register.role === 'student'"-->
+<!--        id="fieldsetHorizontal">-->
+<!--        <b-form-select v-model="register.gender" :options="genderOptions" size="md" id="role" class="w-100"></b-form-select>-->
+<!--      </b-form-group>-->
+
+      <label class="smaller-font">Security Question</label>
+      <b-form-select v-model="register.security" :options="securityOptions" size="md" id="security" class="w-100"></b-form-select>
+      <label class="smaller-font">Security Question Answer</label>
+      <b-form-input id="security_answer" placeholder="Please input your answer here"
+                    v-model.trim="register.security_answer" class="no-border" style="margin-bottom: 10px"></b-form-input>
+
       <button type="submit" class="mt-2 btn-outline-primary" style="width: 100%; height: 35px; border-radius: 10px">Register</button>
       <br />
     </b-form>
@@ -68,10 +66,19 @@ export default {
     return {
       register: {
         role: 'student',
-        gender: 'I do not wish to specify'
+        gender: 'I do not wish to specify',
+        security:'What city were you born in?',
+        security_answer:null
       },
       error: '',
       state: null,
+       securityOptions:[
+        {value: 'What city were you born in?', text: 'What city were you born in?'},
+        {value: 'What is your mother’s maiden name??', text: 'What is your mother’s maiden name?'},
+        {value: 'What is your favorite food?', text: 'What is your favorite food?'},
+        {value: 'Where is your favorite place to vacation?', text: 'Where is your favorite place to vacation?'},
+        {value: 'Where did you go to high school?', text: 'Where did you go to high school?'},
+      ],
       genderOptions: [
         {value: 'I do not wish to specify', text: 'I do not wish to specify'},
         {value: 'Male', text: 'Male'},
@@ -80,7 +87,8 @@ export default {
       options: [
         { value: 'student', text: 'Student' },
         { value: 'employer', text: 'Employer' }
-      ]
+      ],
+      on_typing:false,
     }
   },
   watch: {
@@ -91,6 +99,9 @@ export default {
     }
   },
   methods: {
+    showPassRequirement(){
+      this.on_typing=true
+    },
     onSubmit (evt) {
       this.error = ''
       evt.preventDefault()
@@ -156,6 +167,8 @@ export default {
   }
 
   .smaller-font {
+    padding-top: 10px;
+    margin-bottom: 0px;
     font-size: 13px;
   }
 
