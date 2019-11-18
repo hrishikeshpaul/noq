552cc8205270c7826efb2cea6f9a164dbc592b39
hrishikeshpaul <hrishikeshpaul12@gmail.com>
2019-11-18 01:49:09 -0500

    remove unused spinners

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -15,12 +15,6 @@
       </div>
     </nav>
 
-    <div class="spinner-grow text-primary" role="status">
-      <span class="sr-only">Loading...</span>
-    </div>
-    <div class="spinner-grow text-secondary" role="status">
-      <span class="sr-only">Loading...</span>
-    </div>
     <div class="mx-5 mt-4">
       <div v-for="(job, key) in computedJobs" class="mb-3" v-if="userRole === 'student'" >
         <div style="position: relative;">
